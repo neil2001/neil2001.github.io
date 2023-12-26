9032de37d31d5cf4da71879505c5fd7997845bf7
Neil Xu <neilxu@Neils-MBP.attlocal.net>
2023-12-26 17:48:27 -0600

    minor fixes

--- src/assets/components/navbar.js
+++ src/assets/components/navbar.js
@@ -5,34 +5,34 @@ const NavBar = () => {
   return (
     <div className="sidebar">
       <div className="container">
-      <img className="portrait" src={require('../images/portrait.jpeg')}></img>
-        
-      <div className="name">
-      <a href="/">
-      <h1>Neil Xu</h1>
-      </a>
-      <h2>Applied Math + CS @ Brown</h2>
-      </div>
-    <ul className="nav">
-      <li><a href="#about">About</a></li>
-      <li>
-        <a href="#experience">Experience</a>
-      </li>
-      <li>
-        <a href="#projects">Projects</a>
-      </li>
-    </ul>
-    <div className="footer">
-      <a href="https://github.com/neil2001" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-github fa-lg"></i>
-      </a>
-      <a href="https://www.linkedin.com/in/neil-xu-2401/" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-linkedin fa-lg"></i>
-      </a>
-      <a href="mailto:neil_xu@brown.edu" id="email">
-        <i className="fas fa-envelope fa-lg"></i>
-      </a>
-    </div>
+        <img className="portrait" src={require('../images/portrait.jpeg')}></img>
+          
+        <div className="name">
+          <a href="/">
+          <h1>Neil Xu</h1>
+          </a>
+          <h2>Applied Math + CS @ Brown</h2>
+        </div>
+        <ul className="nav">
+          <li><a href="#about">About</a></li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+        </ul>
+        <div className="footer">
+          <a href="https://github.com/neil2001" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github fa-lg"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/neil-xu-2401/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin fa-lg"></i>
+          </a>
+          <a href="mailto:neil_xu@brown.edu" id="email">
+            <i className="fas fa-envelope fa-lg"></i>
+          </a>
+        </div>
     </div>
   </div>
   );
